1cf18803639e7f2939abbe261fc0bc8e9aca1c25
SimoneFranzoni <simofranzoni99@gmail.com>
2021-12-21 16:17:07 +0100

    snack

--- Snack4/index.php
+++ Snack4/index.php
@@ -9,7 +9,7 @@ function randomNumberArray($min, $max, $nItems){
     if($min > $max){
         $tempMin = $min;
         $min = $max;
-        $max = $tempMIn
+        $max = $tempMin;
     };
 
     if(($max - $min) < $nItems){
@@ -17,6 +17,27 @@ function randomNumberArray($min, $max, $nItems){
     };
 
     while(count($newArray) < $nItems){
+        $number = rand($min, $max);
+        if(!in_array($number, $newArray)) {
+            $newArray[] = $number;
+        }
 
-    }
-}
+    };
+
+    return $newArray;
+};
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack4</title>
+</head>
+<body>
+    
+</body>
+</html>
